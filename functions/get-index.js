7fa933efd55dc76efb131927071c8ec7cae5a70f
theburningmonk <theburningmonk@gmail.com>
2018-07-15 02:08:32 +0100

    - updated UI to support placing order

--- functions/get-index.js
+++ functions/get-index.js
@@ -6,6 +6,7 @@ const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const ordersApiRoot = process.env.orders_api
 
 const awsRegion = process.env.AWS_REGION
 const cognitoUserPoolId = process.env.cognito_user_pool_id
@@ -55,7 +56,8 @@ module.exports.handler = async (event, context) => {
     cognitoClientId,
     dayOfWeek, 
     restaurants,
-    searchUrl: `${restaurantsApiRoot}/search`
+    searchUrl: `${restaurantsApiRoot}/search`,
+    placeOrderUrl: `${ordersApiRoot}`
   }
   const html = Mustache.render(template, view)
   const response = {
